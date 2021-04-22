56bc52709bc4f9d3ed00c4b8355987191fef459a
CFriday42 <80791074+CFriday42@users.noreply.github.com>
2021-04-21 21:27:19 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Correct
+//
+//  Created by Carol Friday on 4/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
